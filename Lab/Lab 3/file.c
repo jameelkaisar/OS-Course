b350bc96b34c587b2bdefb3054a2f3f7877c3166
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2022-04-19 08:52:49 +0530

    Add Lab 1 to 8

---
+++ Lab/Lab 3/file.c
@@ -0,0 +1,21 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    // create
+    int fd = creat("test.txt", S_IRWXG);
+    if (fd == -1)
+        printf("Cannot create file!\n");
+    else
+        printf("File created sucessfully\n");
+
+    // close
+    if (close(fd) == -1)
+        printf("Cannot close file!\n");
+    else
+        printf("File closed sucessfully\n");
+
+    return 0;
+}
